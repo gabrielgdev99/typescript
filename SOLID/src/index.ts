2bb9dfc9fdc54518e3bdc1e46a832bb27a15b519
Gabriel Gonçalves <gabrielgdev99@outlook.com>
2025-11-26 21:13:21 -0300

    Starting JEST

---
+++ SOLID/src/index.ts
@@ -0,0 +1,20 @@
+/*
+
+Vantagens e desvantagens do S.O.L.I.D
+
+Vantagens:
+- Codigo modular
+- Codigo reutilizavel (D.R.Y - Dont repeat yourself)
+- Codigo testavel, baixo acoplamento
+- Baixo acoplamento e alta coesão
+- Codigo expansivel
+- Separations of concerns (separação de conceitos)
+- Facil manutenção
+
+Desvantangens:
+- Complexidade
+- Quantidade de código digitado aumenta
+- Tempo de desenvolvimento aumenta bastante
+- Tenha cuidados com: YAGNI | KISS (You aren't gonna need it | Keep it simple, stupid!)
+
+*/
