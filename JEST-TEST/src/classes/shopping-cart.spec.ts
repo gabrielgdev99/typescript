0278d8505618638e6bde73e5dfc9ce341552a76c
Gabriel Gonçalves <gabrielgdev99@outlook.com>
2025-11-28 14:47:06 -0300

    Finishing pomodoro timer

--- JEST-TEST/src/classes/shopping-cart.spec.ts
+++ JEST-TEST/src/classes/shopping-cart.spec.ts
@@ -1,1 +1,83 @@
+import { Discount } from './discount';
+import { ShoppingCart } from './shopping-cart';
+import { CartItem } from '../classes/interfaces/cart-item';
 
+const creatSut = () => {
+  const discountMock = createDiscountMock();
+  const sut = new ShoppingCart(discountMock);
+  return { sut, discountMock };
+};
+
+const createDiscountMock = () => {
+  class DiscountMock extends Discount {}
+  return new DiscountMock();
+};
+
+const createCartItem = (name: string, price: number) => {
+  class CartItemMock implements CartItem {
+    constructor(
+      public name: string,
+      public price: number,
+    ) {}
+  }
+
+  return new CartItemMock(name, price);
+};
+
+const createSutWithProducts = () => {
+  const { sut, discountMock } = creatSut();
+  const cartItem1 = createCartItem('Short', 20);
+  const cartItem2 = createCartItem('Pen', 2);
+  sut.addItem(cartItem1);
+  sut.addItem(cartItem2);
+  return { sut, discountMock };
+};
+
+describe('ShoppingCart', () => {
+  it('should be an empty cart when no product is added', () => {
+    const { sut } = creatSut();
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it('should have 2 cart items', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+  });
+
+  it('should test total and totalWithDiscount', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.total()).toBe(22);
+    expect(sut.totalWithDiscount()).toBe(22);
+  });
+
+  it('should add products and clear cart', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+    sut.clear();
+    expect(sut.items.length).toBe(0);
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it('should remove products', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+    sut.removeItem(1);
+    expect(sut.items.length).toBe(1);
+    sut.removeItem(0);
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it('should call discount.calculate once when totalWithDiscount is called', () => {
+    const { sut, discountMock } = createSutWithProducts();
+    const discountMockSpy = jest.spyOn(discountMock, 'calculate');
+    sut.totalWithDiscount();
+    expect(discountMockSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call discount.calculate with total price when totalWithDiscount is called', () => {
+    const { sut, discountMock } = createSutWithProducts();
+    const discountMockSpy = jest.spyOn(discountMock, 'calculate');
+    sut.totalWithDiscount();
+    expect(discountMockSpy).toHaveBeenCalledWith(sut.total());
+  });
+});
--- ts-pomodoro-react/src/components/pomodoro-timer.tsx
+++ ts-pomodoro-react/src/components/pomodoro-timer.tsx
@@ -1,7 +1,15 @@
-import React, { JSX } from 'react';
+import React, { JSX, useEffect, useState, useCallback } from 'react';
 import { useInterval } from '../hooks/use-interval';
 import { Button } from './button';
 import { Timer } from './timer';
+import { secondsToTime } from '../utils/seconds-to-minutes';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bellStart = require('../sounds/bell-start.mp3');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bellFinish = require('../sounds/bell-finish.mp3');
+
+const audioStartWorking = new Audio(bellStart);
+const audioFinishWorking = new Audio(bellFinish);
 
 interface Props {
   defaultPomodoroTime: number;
@@ -11,27 +19,111 @@ interface Props {
 }
 
 export function PomodoroTimer(props: Props): JSX.Element {
-  const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime);
+  const [mainTime, setMainTime] = useState(props.defaultPomodoroTime);
+  const [timeCounting, setTimeCounting] = useState(false);
+  const [working, setWorking] = useState(false);
+  const [resting, setResting] = useState(false);
+  const [cyclesQtdManager, setCyclesQtdManager] = useState(
+    new Array(props.cycles - 1).fill(true),
+  );
+  const [completedCycles, setCompletedCycles] = useState(0);
+  const [fullWorkingTime, setfullWorkingTime] = useState(0);
+  const [numberOfPomodoros, setnumberOfPomodoros] = useState(0);
 
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+      if (working) setfullWorkingTime(fullWorkingTime + 1);
+    },
+    timeCounting ? 1000 : null,
+  );
+
+  const configureWork = useCallback(() => {
+    setTimeCounting(true);
+    setWorking(true);
+    setResting(false);
+    setMainTime(props.defaultPomodoroTime);
+    audioStartWorking.play();
+  }, [
+    setTimeCounting,
+    setWorking,
+    setResting,
+    setMainTime,
+    props.defaultPomodoroTime,
+  ]);
+
+  const configureResting = useCallback(
+    (long: boolean) => {
+      setTimeCounting(true);
+      setWorking(false);
+      setResting(true);
+
+      if (long) {
+        setMainTime(props.longRestTime);
+      } else {
+        setMainTime(props.shortRestTime);
+      }
+
+      audioFinishWorking.play();
+    },
+    [
+      setTimeCounting,
+      setWorking,
+      setResting,
+      setMainTime,
+      props.longRestTime,
+      props.shortRestTime,
+    ],
+  );
+
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+    if (resting) document.body.classList.remove('working');
+
+    if (mainTime > 0) return;
+
+    if (working && cyclesQtdManager.length > 0) {
+      configureResting(false);
+      cyclesQtdManager.pop();
+    } else if (working && cyclesQtdManager.length <= 0) {
+      configureResting(true);
+      setCyclesQtdManager(new Array(props.cycles - 1).fill(true));
+      setCompletedCycles(completedCycles + 1);
+    }
+
+    if (working) setnumberOfPomodoros(numberOfPomodoros + 1);
+    if (resting) configureWork();
+  }, [
+    working,
+    resting,
+    mainTime,
+    cyclesQtdManager,
+    numberOfPomodoros,
+    configureResting,
+    setCyclesQtdManager,
+    configureWork,
+    props.cycles,
+  ]);
+
   return (
     <div className="pomodoro">
-      <h2>You are: working</h2>
+      <h2>Você está: {working ? 'Trabalhando' : 'Descansando'}</h2>
       <Timer mainTime={mainTime} />
 
       <div className="controls">
-        <Button text="teste" onClick={() => console.log(1)}></Button>
-        <Button text="teste" onClick={() => console.log(1)}></Button>
-        <Button text="teste" onClick={() => console.log(1)}></Button>
+        <Button text="Work" onClick={() => configureWork()}></Button>
+        <Button text="Rest" onClick={() => configureResting(false)}></Button>
+        <Button
+          className={!working && !resting ? 'hidden' : ''}
+          text={timeCounting ? 'Pause' : 'Play'}
+          onClick={() => setTimeCounting(!timeCounting)}
+        ></Button>
       </div>
 
       <div className="details">
-        <p>DETAILSSSSSSSSSSSSSSSS</p>
-        <p>DETAILSSSSSSSSSSSSSSSS</p>
-        <p>DETAILSSSSSSSSSSSSSSSS</p>
-        <p>DETAILSSSSSSSSSSSSSSSS</p>
+        <p>Ciclos concluídos: {completedCycles}</p>
+        <p>Horas trabalhadas: {secondsToTime(fullWorkingTime)}</p>
+        <p>Pomodoros concluídos: {numberOfPomodoros}</p>
       </div>
     </div>
   );
