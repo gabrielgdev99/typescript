c72514bc8d0b928e19e5f1f072bc0bb97c086abc
Gabriel Gonçalves <gabrielgdev99@outlook.com>
2025-12-09 17:22:40 -0300

    maincontainer css update

--- blog-next/src/components/MainContainer/styled.ts
+++ blog-next/src/components/MainContainer/styled.ts
@@ -3,7 +3,7 @@ import styled, { css } from "styled-components";
 export const Container = styled.main`
   ${({ theme }) => css`
     max-width: 96rem;
-    font-size: ${theme.font.sizes.medium}
+    font-size: ${theme.font.sizes.medium};
     margin: 0 auto;
     padding: ${theme.spacings.medium};
     display: flex;
