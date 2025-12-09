1a16c3b58dfe38bce9e0983395413f9a5471d58e
Gabriel Gonçalves <gabrielgdev99@outlook.com>
2025-12-09 15:46:18 -0300

    att markdown

--- blog-next/src/utils/markdown-to-html.ts
+++ blog-next/src/utils/markdown-to-html.ts
@@ -3,5 +3,5 @@ import remarkHtml from "remark-html";
 
 export const markdownToHtml = async (content: string): Promise<string> => {
   const data = await remark().use(remarkHtml).process(content);
-  return String(data.contents);
+  return String(data.value);
 };
